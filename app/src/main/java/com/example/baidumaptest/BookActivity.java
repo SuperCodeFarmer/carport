56407d07864b0e6fa9bf7961e76a82df7fff4f5b
yaoliangyong <yaoliangyong221@163.com>
2018-12-16 22:26:47 +0800

    12.16 yly second commit

--- app/src/main/java/com/example/baidumaptest/BookActivity.java
+++ app/src/main/java/com/example/baidumaptest/BookActivity.java
@@ -1,6 +1,7 @@
 package com.example.baidumaptest;
 
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -33,40 +34,93 @@ public class BookActivity extends AppCompatActivity{
             @Override
             public void onItemClickListener(View view) {
                 int position = recyclerView.getChildAdapterPosition(view);
+                BookItem bookItem=listBook.get(position);
                 switch (position) {
                     case 0:
-                        startActivity(new Intent(BookActivity.this, Carport_Details.class));
+                        Intent intent1=new Intent(BookActivity.this, Carport_Details.class);
+                        intent1.putExtra("address",bookItem.getCarportAddress());
+                        intent1.putExtra("timeout",bookItem.getCarportTimeout());
+                        intent1.putExtra("carportimage",
+                                BitmapFactory.decodeResource(getResources(),bookItem.getCarportImage()));
+                        intent1.putExtra("carportcost",bookItem.getCarportCost());
+                        startActivity(intent1);
                         break;
                     case 1:
-                        startActivity(new Intent(BookActivity.this, Carport_Details.class));
+                        Intent intent2=new Intent(BookActivity.this, Carport_Details.class);
+                        intent2.putExtra("address",bookItem.getCarportAddress());
+                        intent2.putExtra("timeout",bookItem.getCarportTimeout());
+                        intent2.putExtra("carportimage",
+                                BitmapFactory.decodeResource(getResources(),bookItem.getCarportImage()));
+                        intent2.putExtra("carportcost",bookItem.getCarportCost());
+                        startActivity(intent2);
                         break;
                     case 2:
-                        startActivity(new Intent(BookActivity.this, Carport_Details.class));
+                        Intent intent3=new Intent(BookActivity.this, Carport_Details.class);
+                        intent3.putExtra("address",bookItem.getCarportAddress());
+                        intent3.putExtra("timeout",bookItem.getCarportTimeout());
+                        intent3.putExtra("carportimage",
+                                BitmapFactory.decodeResource(getResources(),bookItem.getCarportImage()));
+                        intent3.putExtra("carportcost",bookItem.getCarportCost());
+                        startActivity(intent3);
                         break;
                     case 3:
-                        startActivity(new Intent(BookActivity.this, Carport_Details.class));
+                        Intent intent4=new Intent(BookActivity.this, Carport_Details.class);
+                        intent4.putExtra("address",bookItem.getCarportAddress());
+                        intent4.putExtra("timeout",bookItem.getCarportTimeout());
+                        intent4.putExtra("carportimage",
+                                BitmapFactory.decodeResource(getResources(),bookItem.getCarportImage()));
+                        intent4.putExtra("carportcost",bookItem.getCarportCost());
+                        startActivity(intent4);
                         break;
                     case 4:
-                        startActivity(new Intent(BookActivity.this, Carport_Details.class));
+                        Intent intent5=new Intent(BookActivity.this, Carport_Details.class);
+                        intent5.putExtra("address",bookItem.getCarportAddress());
+                        intent5.putExtra("timeout",bookItem.getCarportTimeout());
+                        intent5.putExtra("carportimage",
+                                BitmapFactory.decodeResource(getResources(),bookItem.getCarportImage()));
+                        intent5.putExtra("carportcost",bookItem.getCarportCost());
+                        startActivity(intent5);
                         break;
                     case 5:
-                        startActivity(new Intent(BookActivity.this, Carport_Details.class));
+                        Intent intent6=new Intent(BookActivity.this, Carport_Details.class);
+                        intent6.putExtra("address",bookItem.getCarportAddress());
+                        intent6.putExtra("timeout",bookItem.getCarportTimeout());
+                        intent6.putExtra("carportimage",
+                                BitmapFactory.decodeResource(getResources(),bookItem.getCarportImage()));
+                        intent6.putExtra("carportcost",bookItem.getCarportCost());
+                        startActivity(intent6);
                         break;
                     case 6:
-                        startActivity(new Intent(BookActivity.this, Carport_Details.class));
+                        Intent intent7=new Intent(BookActivity.this, Carport_Details.class);
+                        intent7.putExtra("address",bookItem.getCarportAddress());
+                        intent7.putExtra("timeout",bookItem.getCarportTimeout());
+                        intent7.putExtra("carportimage",
+                                BitmapFactory.decodeResource(getResources(),bookItem.getCarportImage()));
+                        intent7.putExtra("carportcost",bookItem.getCarportCost());
+                        startActivity(intent7);
                         break;
-                    case 7:
-                        startActivity(new Intent(BookActivity.this, Carport_Details.class));
-                        break;
+                        default:
+                            break;
+
                 }
             }
         });
     }
     public void initData(){
-        for (int i=0;i<=6;i++){
-            BookItem bookItem1=new BookItem(R.drawable.th_qq,"东王庄小区","30元/小时","4小时");
+            BookItem bookItem1=new BookItem(R.drawable.th_qq,"东王庄小区","10元/小时","4小时");
             listBook.add(bookItem1);
-        }
+            BookItem bookItem2=new BookItem(R.drawable.th_qq,"矿大家属院","20元/小时","6小时");
+            listBook.add(bookItem2);
+            BookItem bookItem3=new BookItem(R.drawable.th_qq,"丰台科技园","30元/小时","1小时");
+            listBook.add(bookItem3);
+            BookItem bookItem4=new BookItem(R.drawable.th_qq,"五道口地铁站","40元/小时","8小时");
+            listBook.add(bookItem4);
+            BookItem bookItem5=new BookItem(R.drawable.th_qq,"太阳宫大厦","50元/小时","2小时");
+            listBook.add(bookItem5);
+            BookItem bookItem6=new BookItem(R.drawable.th_qq,"西二旗地铁站","60元/小时","8小时");
+            listBook.add(bookItem6);
+            BookItem bookItem7=new BookItem(R.drawable.th_qq,"成府路口西","70元/小时","5小时");
+            listBook.add(bookItem7);
     }
 
 }
--- app/src/main/java/com/example/baidumaptest/Carport_Details.java
+++ app/src/main/java/com/example/baidumaptest/Carport_Details.java
@@ -1,17 +1,52 @@
 package com.example.baidumaptest;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Carport_Details extends AppCompatActivity {
+public class Carport_Details extends AppCompatActivity implements View.OnClickListener {
+    private Button submit;
+    private TextView carportDetailCost;
+    private TextView carportDetailAddress;
+    private ImageView carportImageDetail_1;
+    private ImageView carportImageDetail_2;
+    private ImageView carportImageDetail_3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carport__details);
+        //标题栏
         TextView title= (TextView) findViewById(R.id.title_text);
         title.setText("订单详情");
 
+        carportDetailAddress= (TextView) findViewById(R.id.carport_detail_address);
+        carportDetailCost= (TextView) findViewById(R.id.carport_detail_cost);
+        carportImageDetail_1= (ImageView) findViewById(R.id.image_detail_1);
+        carportImageDetail_2= (ImageView) findViewById(R.id.image_detail_2);
+        carportImageDetail_3= (ImageView) findViewById(R.id.image_detail_3);
+
+        Intent intent=getIntent();
+        carportDetailAddress.setText(intent.getStringExtra("address"));
+        carportDetailCost.setText(intent.getStringExtra("carportcost"));
+        carportImageDetail_1.setImageBitmap((Bitmap) intent.getParcelableExtra("carportimage"));
+        submit= (Button) findViewById(R.id.submit_button);
+        submit.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.submit_button:
+                Toast.makeText(this,"订单已经提交，请前往个人中心查看订单详情",Toast.LENGTH_LONG).show();;
+                break;
+        }
     }
 }
